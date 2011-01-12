46e150f48905d8ad1c46e47ad88dfb3195aab07c
Blender3D <452469+Blender3D@users.noreply.github.com>
2011-01-11 22:29:42 -0500

    Epic new theme ;)

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function()
+{
+  $('#navigation li').click(function()
+  {
+    $(this).siblings().each(function()
+    {
+      $(this).removeClass('selected');
+      $('#' + $(this).text().toLowerCase().replace(' ', '_')).hide();
+    });
+    
+    $(this).addClass('selected');
+    $('#' + $(this).text().toLowerCase().replace(' ', '_')).show();
+  });
+  
+  $('#navigation li:eq(0)').trigger('click');
+});
