15565ce947ba328e80e909a3b206afc78d2c0365
Blender3D <452469+Blender3D@users.noreply.github.com>
2012-05-11 01:56:21 -0400

    Added screenshots.

--- ui/project_files.py
+++ ui/project_files.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/project_files.ui'
 #
-# Created: Tue May  8 21:04:16 2012
+# Created: Fri May 11 01:53:54 2012
 #      by: PyQt4 UI code generator 4.9.1
 #
 # WARNING! All changes made in this file will be lost!
